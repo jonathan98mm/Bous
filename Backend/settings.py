c72be47f59ac8eaed130d4315144b3183e757530
jonathan98mm <45021990+jonathan98mm@users.noreply.github.com>
2021-07-07 19:53:16 -0500

    Configuracion de Django para usar PostgreSQL

--- Backend/settings.py
+++ Backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lkind6fak3v73mj3*a-4i@fl4*tn@n69tswfo6!zv!rvwgc^qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
